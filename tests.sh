cf88f7c895284de12e735995b156bbd0eb483aa3
Ryan Moeller <ryan@freqlabs.com>
2018-04-18 23:15:33 -0700

    Additional tests

--- tests.sh
+++ tests.sh
@@ -47,9 +47,53 @@ do
     tests="${tests} ${name}"
 done
 
+#-----------------------------------------
+
+atf_test_case H3oc1
+
+H3oc1_head() {
+    atf_set descr H3oc1.ss
+    atf_set require.files $(atf_get_srcdir)/tests/H3oc1.ss
+    atf_set require.progs $(atf_get_srcdir)/P2.out
+}
+
+H3oc1_body() {
+    cp $(atf_get_srcdir)/tests/H3oc1.ss test.ss
+    atf_check -o ignore -e ignore $(atf_get_srcdir)/P2.out test.ss
+    atf_check diff -wu $(atf_get_srcdir)/tests/H3oc1.p2 test.p2
+}
+
+H3oc1_cleanup() {
+    rm test.p1 test.p2 test.lst test.dbg test.ss
+}
+
+#-----------------------------------------
+
+atf_test_case H3oc2
+
+H3oc2_head() {
+    atf_set descr H3oc2.ss
+    atf_set require.files $(atf_get_srcdir)/tests/H3oc2.ss
+    atf_set require.progs $(atf_get_srcdir)/P2.out
+}
+
+H3oc2_body() {
+    cp $(atf_get_srcdir)/tests/H3oc2.ss test.ss
+    atf_check -o ignore -e ignore $(atf_get_srcdir)/P2.out test.ss
+    atf_check diff -wu $(atf_get_srcdir)/tests/H3oc2.p2 test.p2
+}
+
+H3oc2_cleanup() {
+    rm test.p1 test.p2 test.lst test.dbg test.ss
+}
+
+#-----------------------------------------
+
 atf_init_test_cases() {
     for t in ${tests}
     do
         atf_add_test_case ${t}
     done
+    atf_add_test_case H3oc1
+    atf_add_test_case H3oc2
 }
